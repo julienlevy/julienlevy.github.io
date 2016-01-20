21015052113802e9db80afa9b74c6bf6bc00c244
Julien Levy <julienlevy@ymail.com>
2016-01-20 17:13:16 +0100

    fix

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+// function init() {
+//   var height = $(window).height();
+//   // $('.homepage').css({'min-height': height - 80});
+// }
+//
+// $( window ).resize(function() {
+//   init();
+// });
+//
+// init();
